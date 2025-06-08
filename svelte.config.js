560e87277c8992d5b1c6c8b17712d26448b94cf4
Cheap Nightbot <151442433+CheapNightbot@users.noreply.github.com>
2025-06-09 08:20:34 +0900

    Update svelte.config.js
    - Add CSP config.

--- svelte.config.js
+++ svelte.config.js
@@ -3,7 +3,18 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 const config = {
 	preprocess: vitePreprocess(),
-	kit: { adapter: adapter() }
+	kit: {
+		adapter: adapter(),
+		csp: {
+			directives: {
+				'script-src': ['self']
+			},
+			reportOnly: {
+				'script-src': ['self'],
+				'report-uri': ['/']
+			}
+		}
+	}
 };
 
 export default config;
