e8ec2068ad80f50b21b141a72f00c84344970b5c
Cheap Nightbot <151442433+CheapNightbot@users.noreply.github.com>
2025-07-10 15:00:32 +0900

    Update +layout.svelte

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,11 @@
 <script lang="ts">
-	import '../app.css';
-	
+	import "../app.css";
+	import Header from "../components/Header.svelte";
+	import Footer from "../components/Footer.svelte";
+
 	let { children } = $props();
 </script>
 
+<Header />
 {@render children()}
+<Footer />
