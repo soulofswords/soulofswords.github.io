586e69b117d7af4e11728c432981e9fb49eb6663
Cheap Nightbot <151442433+CheapNightbot@users.noreply.github.com>
2025-07-18 12:48:15 +0900

    Update +layout.svelte

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,7 +6,7 @@
 	let { children } = $props();
 </script>
 
-<div class="flex flex-col gap-4 min-h-dvh min-w-full border p-6">
+<div class="flex flex-col mx-auto gap-4 min-h-dvh max-w-4/5 p-6">
 	<Header />
 	<main class="flex-1 border rounded-2xl">
 		{@render children()}
