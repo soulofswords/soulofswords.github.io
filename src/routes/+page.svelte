fbfb51104b6fa6abe3db6df171fdba5987619a18
Cheap Nightbot <151442433+CheapNightbot@users.noreply.github.com>
2025-07-18 12:48:12 +0900

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,6 @@
 <section class="flex items-center justify-center py-4">
     <img
-        class="fixed w-fit h-10/14 brightness-0 hover:brightness-[initial] transition-all duration-400 top-1/5"
+        class="fixed w-fit h-10/15 brightness-0 hover:brightness-[initial] transition-all duration-400 ease-in-out top-1/5"
         src="sos.svg"
         alt="soul of swords live2d model"
     />
