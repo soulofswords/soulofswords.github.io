e3e588e9271ed71735fc8c73245110cf1aefbc43
Cheap Nightbot <151442433+CheapNightbot@users.noreply.github.com>
2025-07-25 17:39:48 +0900

    Create +error.svelte

---
+++ src/routes/+error.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+    import { page } from '$app/stores';
+</script>
+
+<section class="min-h-[calc(100dvh-14rem)] md:min-h-[calc(100dvh-12rem)] flex flex-col items-center justify-center">
+    <h1 class="text-8xl font-bold bg-gradient-to-tr from-indigo-300 to-pink-300 bg-clip-text text-transparent mb-2">{$page.status}</h1>
+    <p class="text-3xl">{$page.error?.message}</p>
+</section>
