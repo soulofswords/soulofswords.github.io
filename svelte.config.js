fb6d46ac67ebeddaa90de5013639e7d6f5c4b453
Cheap Nightbot <151442433+CheapNightbot@users.noreply.github.com>
2025-07-25 16:14:15 +0900

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -6,13 +6,17 @@ const config = {
 	// Consult https://svelte.dev/docs/kit/integrations
 	// for more information about preprocessors
 	preprocess: vitePreprocess(),
-	kit: { adapter: adapter({
+	kit: {
+		adapter: adapter({
 			pages: 'build',
 			assets: 'build',
-			fallback: undefined,
+			fallback: '404.html',
 			precompress: false,
 			strict: true
-		})
+		}),
+		paths: {
+			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
+		}
 	}
 };
 
