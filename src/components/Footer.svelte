db64fd9f4d7bd21492fa8b3cc3afb1430032bef7
Cheap Nightbot <151442433+CheapNightbot@users.noreply.github.com>
2025-07-25 15:37:45 +0900

    Update Footer.svelte

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -2,6 +2,7 @@
     const year = new Date().getFullYear();
 </script>
 
-<footer>
+<footer class="flex items-center justify-between px-2 text-sm md:text-base">
     <p>&copy;{year} Soul of Swords</p>
+    <slot />
 </footer>
