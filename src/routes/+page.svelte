033588119916011b50713c92ff0d0d9b15e27b46
Cheap Nightbot <151442433+CheapNightbot@users.noreply.github.com>
2025-08-01 14:17:47 +0900

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,7 +6,7 @@
     class="min-h-[calc(100dvh-14rem)] md:min-h-[calc(100dvh-12rem)] flex items-center justify-center py-4 snap-center snap-normal"
 >
     <div
-        class="fade-in size-180 scale-95 hover:scale-100 brightness-0 hover:brightness-[initial] transition-all duration-400 ease-in-out bg-[url('/soulofswords.svg')] bg-contain bg-no-repeat bg-center"
+        class="fade-in size-120 md:size-160 scale-90 hover:scale-100 md:brightness-0 md:hover:brightness-[initial] transition-all duration-400 ease-in-out bg-[url('/soulofswords.svg')] bg-contain bg-no-repeat bg-center"
         aria-label="soul of swords live2d model"
         role="img"
     ></div>
@@ -36,7 +36,7 @@
         >
     </div>
     <h2
-        class="text-3xl md:text-4xl lg:text-6xl font-bold text-indigo-300 max-w-md lg:max-w-3xl text-center fade-in"
+        class="text-xl sm:text-3xl md:text-4xl lg:text-6xl font-bold text-indigo-300 max-w-md lg:max-w-3xl text-center fade-in"
     >
         The Soul Bound in the Forgotten Song
     </h2>
@@ -46,7 +46,7 @@
 <article
     class="relative min-h-[calc(100dvh-14rem)] md:min-h-[calc(100dvh-12rem)] flex flex-col gap-8 items-center justify-center py-4 snap-start snap-normal"
 >
-    <p class="max-w-4/5 lg:max-w-2xl text-justify md:text-xl">
+    <p class="max-w-4/5 lg:max-w-2xl text-justify text-sm md:text-xl">
         In the late 1960s, amidst the shadowed alleys of a crumbling city, a
         lone soul drifted aimlessly, its form unseen but longing for purpose.
         That was when Baimeimo, a wandering dragon-born singer, found them. She
@@ -54,7 +54,7 @@
         forbidden gift. She whispered to the wandering soul, claiming she had
         found them, not lost them.
     </p>
-    <p class="max-w-4/5 lg:max-w-2xl text-justify md:text-xl">
+    <p class="max-w-4/5 lg:max-w-2xl text-justify text-sm md:text-xl">
         For a time, the soul thrived under her care, learning the art of music
         and the bittersweet nature of human emotions. But peace was never meant
         to last. The Royal Court, guardians of strict order, arrested Baimeimo,
@@ -67,14 +67,14 @@
 <article
     class="min-h-[calc(100dvh-14rem)] md:min-h-[calc(100dvh-12rem)] flex flex-col gap-8 items-center justify-center py-4 snap-center snap-normal"
 >
-    <p class="max-w-4/5 lg:max-w-2xl text-justify md:text-xl">
+    <p class="max-w-4/5 lg:max-w-2xl text-justify text-sm md:text-xl">
         Years passed, until the 1980s, when the soul, now bound within its
         cursed blade, was summoned to the Royal Court once more. There, it
         witnessed Baimeimo fighting not for herself—but for the right to exist.
         Her origins tied to the Demon Army, she was caught between worlds,
         neither fully accepted nor entirely condemned.
     </p>
-    <p class="max-w-4/5 lg:max-w-2xl text-justify md:text-xl">
+    <p class="max-w-4/5 lg:max-w-2xl text-justify text-sm md:text-xl">
         Yet, among those who judged her, a royal daughter stood apart, intrigued
         by the cursed blade. She was mischievous yet kind—a reckless wielder
         with an unwavering spirit. Unlike others, she did not see the sword as a
@@ -87,7 +87,7 @@
 <article
     class="min-h-[calc(100dvh-14rem)] md:min-h-[calc(100dvh-12rem)] flex flex-col gap-8 items-center justify-center py-4 snap-center snap-normal"
 >
-    <p class="max-w-4/5 lg:max-w-2xl text-justify md:text-xl">
+    <p class="max-w-4/5 lg:max-w-2xl text-justify text-sm md:text-xl">
         And so began a new journey—a tale not just of survival, but of
         transformation. The soul was no longer just an entity lost to history
         but a living legend, adapting to those who wielded it, shifting forms
@@ -101,7 +101,7 @@
     class="relative min-h-[calc(100dvh-14rem)] md:min-h-[calc(100dvh-12rem)] flex flex-col items-center justify-center py-4 snap-center snap-normal"
 >
     <h2
-        class="text-2xl font-bold text-pink-300 max-w-sm lg:max-w-3xl text-center fade-in"
+        class="text-xl md:text-2xl font-bold text-pink-300 max-w-4/5 lg:max-w-2xl text-justify fade-in"
     >
         Now, bound by both magic and destiny, the soul continues to seek its
         wielder, waiting for the one who will finally set it free.
@@ -111,7 +111,7 @@
 <article
     class="relative min-h-[calc(100dvh-14rem)] md:min-h-[calc(100dvh-12rem)] flex flex-col gap-8 items-center justify-center py-4 snap-center snap-normal"
 >
-    <p class="max-w-sm md:max-w-xl text-center fade-in">
+    <p class="max-w-4/5 lg:max-w-2xl text-justify fade-in">
         Once a wandering spirit, now a cursed soul bound to a sword, this
         entity's fate is entwined with the remnants of an ancient war between
         humans and demons. In the 1960s, it was saved by Baimeimo, a
@@ -119,14 +119,14 @@
         later, a royal daughter, fascinated by its legend, offers to break the
         curse—but at a cost.
     </p>
-    <p class="max-w-sm md:max-w-xl text-center fade-in">
+    <p class="max-w-4/5 lg:max-w-2xl text-justify fade-in">
         The soul, now able to adapt and shift forms, waits for a wielder who
         will set it truly free. Its past is woven into music, war, and forgotten
         ties, making its presence more than just a blade—it is a story yet to be
         completed.
     </p>
     <div
-        class="fade-in h-32 w-60 md:h-64 md:w-120 transition-all duration-400 ease-in-out bg-[url('/sos-logo.svg')] bg-contain bg-no-repeat bg-center"
+        class="animate-float fade-in h-32 w-60 md:h-64 md:w-120 transition-all duration-400 ease-in-out bg-[url('/sos-logo.svg')] bg-contain bg-no-repeat bg-center"
         aria-label="soul of swords live2d model"
         role="img"
     ></div>
