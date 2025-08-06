1f25ca547db45c074df4f01856ecb9c87a77c64f
Cheap Nightbot <151442433+CheapNightbot@users.noreply.github.com>
2025-08-06 18:55:49 +0900

    Hide Scroll Down Arrow on scroll down

--- src/lib/components/ScrollDown.svelte
+++ src/lib/components/ScrollDown.svelte
@@ -1,4 +1,4 @@
-<p class="fade-in">
+<p class="fade-in scroll-down">
     <svg
         class="size-12 animate-bounce text-pink-300"
         xmlns="http://www.w3.org/2000/svg"
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -16,6 +16,12 @@
 <div class="flex flex-col mx-auto gap-4 md:max-w-4/5 max-h-dvh p-6">
 	<Header />
 	<main
+		onscroll={(e) => {
+			const scorllDownArrow = document.querySelector(".scroll-down");
+			e.currentTarget.scrollTop > 200
+				? scorllDownArrow?.classList.add("fade-out")
+				: scorllDownArrow?.classList.remove("fade-out");
+		}}
 		class="relative flex-1 flex flex-col gap-12 border rounded-2xl overflow-x-hidden overflow-y-scroll scroll-smooth snap-y snap-mandatory min-h-[calc(100dvh-14rem)] md:min-h-[calc(100dvh-12rem)] max-h-1/2"
 		id="main-content"
 	>
