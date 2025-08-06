51f512e85ddc2dcce867698ea3ec2cc800c4301d
Cheap Nightbot <151442433+CheapNightbot@users.noreply.github.com>
2025-08-06 18:10:29 +0900

    Update +error.svelte

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -2,7 +2,9 @@
     import { page } from '$app/stores';
 </script>
 
-<section class="min-h-[calc(100dvh-14rem)] md:min-h-[calc(100dvh-12rem)] flex flex-col items-center justify-center">
+<section
+    class="relative min-h-[calc(100dvh-14rem)] md:min-h-[calc(100dvh-10rem)] flex flex-col items-center justify-center py-4 snap-center snap-normal"
+>
     <h1 class="text-8xl font-bold bg-gradient-to-tr from-indigo-300 to-pink-300 bg-clip-text text-transparent mb-2">{$page.status}</h1>
     <p class="text-3xl">{$page.error?.message}</p>
 </section>
