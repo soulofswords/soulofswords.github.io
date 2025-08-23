d3c6a5e50f2c02def4556d7c575d41133b5e163d
Cheap Nightbot <151442433+CheapNightbot@users.noreply.github.com>
2025-08-23 23:35:45 +0900

    Create header-separater.svelte

---
+++ src/lib/components/header-separater.svelte
@@ -0,0 +1,35 @@
+<div class="flex mb-2">
+    <div class="content-center animate-spin">
+        <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            class="size-6"
+        >
+            <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M12 4.5v15m7.5-7.5h-15"
+            />
+        </svg>
+    </div>
+    <div class="border-t flex-1 relative top-3 mx-4"></div>
+    <div class="content-center animate-spin">
+        <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            class="size-6"
+        >
+            <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M12 4.5v15m7.5-7.5h-15"
+            />
+        </svg>
+    </div>
+</div>
