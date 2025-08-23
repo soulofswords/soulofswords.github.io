1ecc8564d7a3590b4654860a6c9c0c0cfdc3e25c
Cheap Nightbot <151442433+CheapNightbot@users.noreply.github.com>
2025-08-23 22:34:12 +0900

    Create footer.svelte

---
+++ src/lib/components/footer.svelte
@@ -0,0 +1,95 @@
+<script lang="ts">
+    const year = new Date().getFullYear();
+</script>
+
+<footer
+    class="flex flex-col md:flex-row items-center justify-between gap-2 p-4 text-sm"
+>
+    <p>&copy; {year} Souls of Swords. All rights reserved.</p>
+    <section class="flex items-center space-x-4">
+        <a
+            class="hover:scale-105 hover:text-secondary transition-all duration-200 ease-in-out"
+            href="https://discord.gg/fJKjw9QAUg"
+            rel="noopener noreferrer"
+            target="_blank"
+            aria-label="Discord Server"
+        >
+            <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                ><path
+                    fill="currentColor"
+                    d="M19.27 5.33C17.94 4.71 16.5 4.26 15 4a.1.1 0 0 0-.07.03c-.18.33-.39.76-.53 1.09a16.1 16.1 0 0 0-4.8 0c-.14-.34-.35-.76-.54-1.09c-.01-.02-.04-.03-.07-.03c-1.5.26-2.93.71-4.27 1.33c-.01 0-.02.01-.03.02c-2.72 4.07-3.47 8.03-3.1 11.95c0 .02.01.04.03.05c1.8 1.32 3.53 2.12 5.24 2.65c.03.01.06 0 .07-.02c.4-.55.76-1.13 1.07-1.74c.02-.04 0-.08-.04-.09c-.57-.22-1.11-.48-1.64-.78c-.04-.02-.04-.08-.01-.11c.11-.08.22-.17.33-.25c.02-.02.05-.02.07-.01c3.44 1.57 7.15 1.57 10.55 0c.02-.01.05-.01.07.01c.11.09.22.17.33.26c.04.03.04.09-.01.11c-.52.31-1.07.56-1.64.78c-.04.01-.05.06-.04.09c.32.61.68 1.19 1.07 1.74c.03.01.06.02.09.01c1.72-.53 3.45-1.33 5.25-2.65c.02-.01.03-.03.03-.05c.44-4.53-.73-8.46-3.1-11.95c-.01-.01-.02-.02-.04-.02M8.52 14.91c-1.03 0-1.89-.95-1.89-2.12s.84-2.12 1.89-2.12c1.06 0 1.9.96 1.89 2.12c0 1.17-.84 2.12-1.89 2.12m6.97 0c-1.03 0-1.89-.95-1.89-2.12s.84-2.12 1.89-2.12c1.06 0 1.9.96 1.89 2.12c0 1.17-.83 2.12-1.89 2.12"
+                /></svg
+            >
+        </a>
+
+        <a
+            class="hover:scale-105 hover:text-secondary transition-all duration-200 ease-in-out"
+            href="https://youtube.com/@soulofswords"
+            rel="noopener noreferrer"
+            target="_blank"
+            aria-label="YouTube"
+        >
+            <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+            >
+                <path
+                    fill="currentColor"
+                    fill-rule="evenodd"
+                    d="M1.172 7.693A56 56 0 0 0 1 12c0 1.632.08 3.167.182 4.44c.146 1.817 1.531 3.206 3.318 3.33c1.794.125 4.305.23 7.5.23s5.706-.105 7.5-.23c1.787-.124 3.172-1.513 3.318-3.33c.102-1.273.182-2.808.182-4.44c0-1.576-.074-3.06-.172-4.307a3.73 3.73 0 0 0-3.46-3.46C17.59 4.107 15.164 4 12 4s-5.591.107-7.368.233a3.73 3.73 0 0 0-3.46 3.46M15.5 12L10 9v6z"
+                    clip-rule="evenodd"
+                />
+            </svg>
+        </a>
+
+        <a
+            class="hover:scale-105 hover:text-secondary transition-all duration-200 ease-in-out"
+            href="https://twitch.tv/soulofswords"
+            rel="noopener noreferrer"
+            target="_blank"
+            aria-label="Twitch"
+        >
+            <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="19"
+                height="19"
+                viewBox="0 0 24 24"
+            >
+                <path
+                    fill="currentColor"
+                    fill-rule="evenodd"
+                    d="M3 1L1.5 5v15H7v3h3l3-3h4l5.5-5.5V1zm2 2h15.5v10.5L17 17h-5l-2.5 2.5V17H5zm5 9.5V7h2v5.5zM15 7v5.5h2V7z"
+                    clip-rule="evenodd"
+                />
+            </svg>
+        </a>
+
+        <a
+            class="hover:scale-105 hover:text-secondary transition-all duration-200 ease-in-out"
+            href="https://x.com/soulofswords"
+            rel="noopener noreferrer"
+            target="_blank"
+            aria-label="X (formerly Twitter)"
+        >
+            <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="18"
+                height="18"
+                viewBox="0 0 24 24"
+            >
+                <path
+                    fill="currentColor"
+                    fill-rule="evenodd"
+                    d="m13.458 9.122l7.516-7.965h2.491l-.01.011l-8.89 9.424l8.139 10.802a.906.906 0 0 1-.658 1.45H16.95a.9.9 0 0 1-.659-.359l-5.727-7.601l-7.472 7.96H.535l8.922-9.43L1.318 2.612a.906.906 0 0 1 .724-1.452h4.965c.285 0 .553.134.724.361zm-.763 2a1 1 0 0 1-.07-.093l-6.07-8.056H3.86l13.607 18.06h2.696z"
+                    clip-rule="evenodd"
+                />
+            </svg>
+        </a>
+    </section>
+</footer>
